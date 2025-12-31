9e2648e9139091a5cb922393b01b9bf01aa464ed
np01nt4a250181-afk <np01nt4a250181@islingtoncollege.edu.np>
2025-12-31 10:47:02 +0545

    Add files via upload

---
+++ vehicle.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class vehicle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class vehicle
+{
+    int registrationNumber;
+    String brand;
+    int price;
+    int numberofDoors;
+    
+    
+}
